facf9f217c8aa90ff070a0747e94eadb01996fd6
“zhongyueming1121” <mygamez@163.com>
2021-11-26 01:39:16 +0800

    注释测试类

--- src/test/java/com/ymz/ui/DateTimePickerTest.java
+++ src/test/java/com/ymz/ui/DateTimePickerTest.java
@@ -7,18 +7,5 @@ import java.util.Date;
 
 class DateTimePickerTest {
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setTitle("Date Time Picker");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        DateTimePicker dateTimePicker = new DateTimePicker();
-        dateTimePicker.setFormats(DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM));
-        dateTimePicker.setTimeFormat(DateFormat.getTimeInstance(DateFormat.MEDIUM));
-        dateTimePicker.setDate(new Date());
-
-        frame.getContentPane().add(dateTimePicker);
-        frame.pack();
-        frame.setVisible(true);
-    }
 
 }
--- src/test/java/com/ymz/util/AllUtilsTest.java
+++ src/test/java/com/ymz/util/AllUtilsTest.java
@@ -11,21 +11,21 @@ public class AllUtilsTest {
 
     @Test
     public void foundFile() {
-        String fname = "*iController.java";
+       /* String fname = "*iController.java";
         long t1 = System.currentTimeMillis();
         List<File> files = new ArrayList<>();
         AllUtils.findFiles("G:\\CODE\\cloudeyes-maven", fname, files);
         long t2 = System.currentTimeMillis();
         System.out.println(files.toString());
-        System.out.println(t2 - t1);
+        System.out.println(t2 - t1);*/
     }
 
     @Test
     public void zip() {
-        String srcPath = "G:\\CODE\\svnpatch";
+        /*String srcPath = "G:\\CODE\\svnpatch";
         String dstPath = "G:\\CODE\\svnpatch\\";
         String zipPath = srcPath + File.separator + StringUtils.substringAfterLast(srcPath, File.separator) + ".zip";
         System.out.println(zipPath);
-        AllUtils.zipFileInFolder(srcPath, zipPath);
+        AllUtils.zipFileInFolder(srcPath, zipPath);*/
     }
 }
