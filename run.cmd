2d9b27a5c92b3bcd7a73069ed3c61b192615b53c
“zhongyueming1121” <mygamez@163.com>
2021-11-30 00:16:48 +0800

    cmd

--- run.cmd
+++ run.cmd
@@ -1,6 +1,4 @@
 @echo off
-::will set java_home only in this window.
-::you can uncomment to set JAVA_HOME
 ::set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_271
 ::set CLASSPATH=.;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar
 ::set PATH=%JAVA_HOME%/bin;
